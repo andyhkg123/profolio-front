d4388872ccca61f7e53cbbd2617097519486ad39
Andy Wong <andy.wongtszkiu@gmail.com>
2024-10-27 02:36:57 +0800

    as

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  build: {
+    outDir: "dist",
+  },
+  base: "/dist/", // Ensure this matches your deployment setup
+
   plugins: [react()],
   server: {
     proxy: {
